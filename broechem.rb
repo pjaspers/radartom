901551b2efe82fdae51a96f48171ded5b8b94142
Bob Van Landuyt <bob@vanlanduyt.co>
2015-09-09 08:55:20 +0200

    Simulate the dotter-twitter-xp-rience

--- broechem.rb
+++ broechem.rb
@@ -4,6 +4,12 @@ require "sinatra/base"
 class Broechem < Sinatra::Application
 
   get "/" do
+    @tweet = Tweet.all.sample
+    @reply = dotterisms.sample
     erb :index
+  end
+
+  def dotterisms
+    ["¯\_(ツ)_/¯", "Sure", "Dooooomed", "(╯°□°）╯︵ ┻━┻)"]
   end
 end
