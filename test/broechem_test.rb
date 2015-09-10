a3aadc752d09b59405726aa9732dab01a9d6a062
pjaspers <piet@pjaspers.com>
2015-09-10 23:07:22 +0200

    Add test to show it's broken

--- test/broechem_test.rb
+++ test/broechem_test.rb
@@ -4,6 +4,7 @@ require "minitest/pride"
 require "rack/test"
 require "pry"
 
+ENV['DATABASE_URL'] = 'sqlite://test_dotter.db'
 require File.expand_path "../../broechem.rb", __FILE__
 
 include Rack::Test::Methods
@@ -12,11 +13,32 @@ def app
   Broechem
 end
 
+def setup_tweet
+  Tweet.create(name: "bobvlanduyt", text: "I broke the tests.", tweet_id: "641623559224324096", profile_image: "https://pbs.twimg.com/profile_images/1175219771/Foto_3.jpg")
+end
+
+class Minitest::Test
+  alias_method :_original_run, :run
+
+  def run(*args, &block)
+    result = nil
+    Sequel::Model.db.transaction(:rollback => :always, :auto_savepoint=>true) do
+      result = _original_run(*args, &block)
+    end
+    result
+  end
+end
+
 class AppTest < Minitest::Test
 
   def test_renders_front_page
+    setup_tweet
     get "/"
     assert_equal 200, last_response.status
   end
 
+  def test_renders_front_page_without_tweets
+    get "/"
+    assert_equal 200, last_response.status
+  end
 end
